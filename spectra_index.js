6d142f8a54951087f22cb52a0dc8f7278df4cd99
Tang-jia-jia <1955580782@qq.com>
2026-03-24 14:45:50 +0800

    Add files via upload

---
+++ spectra_index.js
@@ -0,0 +1,494 @@
+const spectraIndex = [
+    {
+        "name": "PI6050 50℃ 100kGy",
+        "formula": "PI6050",
+        "file": null,
+        "is_hot": true
+    },
+    {
+        "name": "PI6050 50℃ 200kGy",
+        "formula": "PI6050",
+        "file": null,
+        "is_hot": true
+    },
+    {
+        "name": "PI6050 50℃ 20kGy",
+        "formula": "PI6050",
+        "file": null,
+        "is_hot": true
+    },
+    {
+        "name": "PI6050 50℃ 50kGy",
+        "formula": "PI6050",
+        "file": null,
+        "is_hot": true
+    },
+    {
+        "name": "PI6050 70℃ 200kGy",
+        "formula": "PI6050",
+        "file": null,
+        "is_hot": true
+    },
+    {
+        "name": "PI6050 70℃ 20kGy",
+        "formula": "PI6050",
+        "file": null,
+        "is_hot": true
+    },
+    {
+        "name": "PI6050 70℃ 50kGy",
+        "formula": "PI6050",
+        "file": null,
+        "is_hot": true
+    },
+    {
+        "name": "PI6050 90℃ 100kGy",
+        "formula": "PI6050",
+        "file": null,
+        "is_hot": true
+    },
+    {
+        "name": "PI6050 90℃ 200kGy",
+        "formula": "PI6050",
+        "file": null,
+        "is_hot": true
+    },
+    {
+        "name": "PI6050 90℃ 20kGy",
+        "formula": "PI6050",
+        "file": null,
+        "is_hot": true
+    },
+    {
+        "name": "PI6050 90℃ 50kGy",
+        "formula": "PI6050",
+        "file": "PI6050_90℃_50kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "PI6050 室温 100kGy",
+        "formula": "PI6050",
+        "file": "PI6050_室温_100kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "PI6050 室温 200kGy",
+        "formula": "PI6050",
+        "file": "PI6050_室温_200kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "PI6050 室温 20kGy",
+        "formula": "PI6050",
+        "file": "PI6050_室温_20kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "PI6050 室温 50kGy",
+        "formula": "PI6050",
+        "file": "PI6050_室温_50kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "PPS 50℃ 100kGy",
+        "formula": "PPS",
+        "file": "PPS_50℃_100kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "PPS 50℃ 200kGy",
+        "formula": "PPS",
+        "file": "PPS_50℃_200kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "PPS 50℃ 20kGy",
+        "formula": "PPS",
+        "file": "PPS_50℃_20kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "PPS 50℃ 50kGy",
+        "formula": "PPS",
+        "file": "PPS_50℃_50kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "PPS 70℃ 200kGy",
+        "formula": "PPS",
+        "file": "PPS_70℃_200kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "PPS 70℃ 20kGy",
+        "formula": "PPS",
+        "file": "PPS_70℃_20kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "PPS 70℃ 50kGy",
+        "formula": "PPS",
+        "file": "PPS_70℃_50kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "PPS 90℃ 100kGy",
+        "formula": "PPS",
+        "file": "PPS_90℃_100kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "PPS 90℃ 200kGy",
+        "formula": "PPS",
+        "file": "PPS_90℃_200kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "PPS 90℃ 20kGy",
+        "formula": "PPS",
+        "file": "PPS_90℃_20kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "PPS 90℃ 50kGy",
+        "formula": "PPS",
+        "file": "PPS_90℃_50kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "PPS 室温 100kGy",
+        "formula": "PPS",
+        "file": "PPS_室温_100kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "PPS 室温 200kGy",
+        "formula": "PPS",
+        "file": "PPS_室温_200kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "PPS 室温 20kGy",
+        "formula": "PPS",
+        "file": "PPS_室温_20kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "PPS 室温 50kGy",
+        "formula": "PPS",
+        "file": "PPS_室温_50kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "丁基橡胶 50℃ 100kGy",
+        "formula": "IIR",
+        "file": "丁基橡胶_50℃_100kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "丁基橡胶 50℃ 200kGy",
+        "formula": "IIR",
+        "file": "丁基橡胶_50℃_200kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "丁基橡胶 50℃ 20kGy",
+        "formula": "IIR",
+        "file": "丁基橡胶_50℃_20kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "丁基橡胶 50℃ 50kGy",
+        "formula": "IIR",
+        "file": "丁基橡胶_50℃_50kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "丁基橡胶 70℃ 200kGy",
+        "formula": "IIR",
+        "file": "丁基橡胶_70℃_200kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "丁基橡胶 70℃ 20kGy",
+        "formula": "IIR",
+        "file": "丁基橡胶_70℃_20kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "丁基橡胶 70℃ 50kGy",
+        "formula": "IIR",
+        "file": "丁基橡胶_70℃_50kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "丁基橡胶 90℃ 100kGy",
+        "formula": "IIR",
+        "file": "丁基橡胶_90℃_100kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "丁基橡胶 90℃ 200kGy",
+        "formula": "IIR",
+        "file": "丁基橡胶_90℃_200kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "丁基橡胶 90℃ 20kGy",
+        "formula": "IIR",
+        "file": "丁基橡胶_90℃_20kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "丁基橡胶 90℃ 50kGy",
+        "formula": "IIR",
+        "file": "丁基橡胶_90℃_50kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "丁基橡胶 室温 100kGy",
+        "formula": "IIR",
+        "file": "丁基橡胶_室温_100kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "丁基橡胶 室温 200kGy",
+        "formula": "IIR",
+        "file": "丁基橡胶_室温_200kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "丁基橡胶 室温 20kGy",
+        "formula": "IIR",
+        "file": "丁基橡胶_室温_20kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "丁基橡胶 室温 50kGy",
+        "formula": "IIR",
+        "file": "丁基橡胶_室温_50kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "丁腈橡胶 50°C 100kGy",
+        "formula": "NBR",
+        "file": "丁腈橡胶_50CC_100kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "丁腈橡胶 50℃ 200kGy",
+        "formula": "NBR",
+        "file": "丁腈橡胶_50℃_200kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "丁腈橡胶 50℃ 20kGy",
+        "formula": "NBR",
+        "file": "丁腈橡胶_50℃_20kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "丁腈橡胶 50℃ 50kGy",
+        "formula": "NBR",
+        "file": "丁腈橡胶_50℃_50kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "丁腈橡胶 70℃ 200kGy",
+        "formula": "NBR",
+        "file": "丁腈橡胶_70℃_200kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "丁腈橡胶 70℃ 20kGy",
+        "formula": "NBR",
+        "file": "丁腈橡胶_70℃_20kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "丁腈橡胶 70℃ 50kGy",
+        "formula": "NBR",
+        "file": "丁腈橡胶_70℃_50kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "丁腈橡胶 90℃ 100kGy",
+        "formula": "NBR",
+        "file": "丁腈橡胶_90℃_100kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "丁腈橡胶 90℃ 200kGy",
+        "formula": "NBR",
+        "file": "丁腈橡胶_90℃_200kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "丁腈橡胶 90℃ 50kGy",
+        "formula": "NBR",
+        "file": "丁腈橡胶_90℃_50kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "丁腈橡胶 室温 100kGy",
+        "formula": "NBR",
+        "file": "丁腈橡胶_室温_100kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "丁腈橡胶 室温 200kGy",
+        "formula": "NBR",
+        "file": "丁腈橡胶_室温_200kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "丁腈橡胶 室温 20kGy",
+        "formula": "NBR",
+        "file": "丁腈橡胶_室温_20kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "丁腈橡胶 室温 50kGy",
+        "formula": "NBR",
+        "file": "丁腈橡胶_室温_50kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "三元乙丙橡胶 50℃ 100kGy",
+        "formula": "EPDM",
+        "file": "三元乙丙橡胶_50℃_100kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "三元乙丙橡胶 50℃ 200kGy",
+        "formula": "EPDM",
+        "file": "三元乙丙橡胶_50℃_200kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "三元乙丙橡胶 50℃ 20kGy",
+        "formula": "EPDM",
+        "file": "三元乙丙橡胶_50℃_20kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "三元乙丙橡胶 50℃ 50kGy",
+        "formula": "EPDM",
+        "file": "三元乙丙橡胶_50℃_50kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "三元乙丙橡胶 70℃ 200kGy",
+        "formula": "EPDM",
+        "file": "三元乙丙橡胶_70℃_200kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "三元乙丙橡胶 70℃ 20kGy",
+        "formula": "EPDM",
+        "file": "三元乙丙橡胶_70℃_20kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "三元乙丙橡胶 70℃ 50kGy",
+        "formula": "EPDM",
+        "file": "三元乙丙橡胶_70℃_50kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "三元乙丙橡胶 90℃ 100kGy",
+        "formula": "EPDM",
+        "file": "三元乙丙橡胶_90℃_100kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "三元乙丙橡胶 90℃ 200kGy",
+        "formula": "EPDM",
+        "file": "三元乙丙橡胶_90℃_200kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "三元乙丙橡胶 90℃ 20kGy",
+        "formula": "EPDM",
+        "file": "三元乙丙橡胶_90℃_20kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "三元乙丙橡胶 90℃ 50kGy",
+        "formula": "EPDM",
+        "file": "三元乙丙橡胶_90℃_50kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "三元乙丙橡胶 室温 100kGy",
+        "formula": "EPDM",
+        "file": "三元乙丙橡胶_室温_100kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "三元乙丙橡胶 室温 200kGy",
+        "formula": "EPDM",
+        "file": "三元乙丙橡胶_室温_200kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "三元乙丙橡胶 室温 20kGy",
+        "formula": "EPDM",
+        "file": "三元乙丙橡胶_室温_20kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "三元乙丙橡胶 室温 50kGy",
+        "formula": "EPDM",
+        "file": "三元乙丙橡胶_室温_50kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "氟橡胶 50℃ 100kGy",
+        "formula": "FKM",
+        "file": "氟橡胶_50℃_100kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "氟橡胶 50℃ 200kGy",
+        "formula": "FKM",
+        "file": "氟橡胶_50℃_200kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "氟橡胶 50℃ 20kGy",
+        "formula": "FKM",
+        "file": "氟橡胶_50℃_20kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "氟橡胶 70℃ 200kGy",
+        "formula": "FKM",
+        "file": "氟橡胶_70℃_200kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "氟橡胶 70℃ 20kGy",
+        "formula": "FKM",
+        "file": "氟橡胶_70℃_20kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "氟橡胶 室温 100kGy",
+        "formula": "FKM",
+        "file": "氟橡胶_室温_100kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "氟橡胶 室温 200kGy",
+        "formula": "FKM",
+        "file": "氟橡胶_室温_200kGy.json",
+        "is_hot": false
+    },
+    {
+        "name": "氟橡胶 室温 20kGy",
+        "formula": "FKM",
+        "file": "氟橡胶_室温_20kGy.json",
+        "is_hot": false
+    }
+];
